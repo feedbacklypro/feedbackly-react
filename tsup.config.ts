f2440cef014ddd0e63a579e4f5c86907e759becb
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2024-10-17 23:13:53 +0545

    tsconfig edit

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["index.ts"],
+  format: ["esm", "cjs"],
   dts: true,
+  splitting: false,
+  sourcemap: true,
   clean: true,
+  treeshake: true,
+  minify: true,
+  target: "es2019",
+  outDir: "dist",
+  external: ["react", "react-dom"],
+  esbuildOptions(options) {
+    options.jsx = "automatic";
+  },
 });
